1c25a41858ee70bda8d51e83d32033ab55dd4a99
Diego <diegolazaro89@gmail.com>
2018-10-18 22:55:47 +0200

    Added 6 e2e tests with protractor

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,14 +1,40 @@
 import { AppPage } from './app.po';
+import { browser } from 'protractor';
+import { AngularWaitBarrier } from 'blocking-proxy/built/lib/angular_wait_barrier';
 
 describe('workspace-project App', () => {
-  let page: AppPage;
+  const page = new AppPage();
 
   beforeEach(() => {
-    page = new AppPage();
+    page.navigateTo();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to brastlewark!');
+  it('should display Brastlewark in the navbar', () => {
+    expect(page.getMainTitle()).toEqual('Brastlewark');
+  });
+  it('should show a gnome\'s name when clicking on one', async () => {
+    await page.clickOnItemByClass('gnome');
+    await page.waitForDialog();
+    expect(page.checkMatListItemExist('Name')).toBeTruthy();
+  });
+  it('should show a gnome\'s age when clicking on one', async () => {
+    await page.clickOnItemByClass('gnome');
+    await page.waitForDialog();
+    expect(page.checkMatListItemExist('Age')).toBeTruthy();
+  });
+  it('should show a gnome\'s hair color when clicking on one', async () => {
+    await page.clickOnItemByClass('gnome');
+    await page.waitForDialog();
+    expect(page.checkMatListItemExist('Hair Color')).toBeTruthy();
+  });
+  it('should show a gnome\'s height when clicking on one', async () => {
+    await page.clickOnItemByClass('gnome');
+    await page.waitForDialog();
+    expect(page.checkMatListItemExist('Height')).toBeTruthy();
+  });
+  it('should show a gnome\'s weight when clicking on one', async () => {
+    await page.clickOnItemByClass('gnome');
+    await page.waitForDialog();
+    expect(page.checkMatListItemExist('Weight')).toBeTruthy();
   });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,37 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ExpectedConditions } from 'protractor';
 
 export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
 
-  getParagraphText() {
+  // Get
+  getMainTitle() {
     return element(by.css('app-root h1')).getText();
+  }
+
+  // Check
+  checkMatListItemExist(item): any {
+    return element.all(by.cssContainingText('.mat-list-item-content', item)).isPresent();
+  }
+
+  // Click
+  clickOnItemByClass(className): any {
+    return element(by.className(className)).click();
+  }
+
+  // Scroll
+  scrollIntoDivClass(className): any {
+    const inputBox = element(by.xpath(`//div[contains(concat(' ', @class, ' '), ' ${className} ')]`));
+    return browser.executeScript('arguments[0].scrollIntoView()', inputBox.getWebElement());
+  }
+
+  // Wait
+  waitForDialog(): any {
+    return this.waitUntilClassVisible('mat-dialog-container');
+  }
+  private waitUntilClassVisible(className): any {
+    const item = element(by.className(className));
+    return browser.wait(ExpectedConditions.visibilityOf(item));
   }
 }
